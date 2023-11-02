cdff91db59d150cebbadaf27bd300eb83309c65a
Moses <mosemuthoni57@gmail.com>
2023-11-02 15:00:01 +0300

    modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        i = i + 1
+        print("{}: {}".format(i, sys.argv[i]))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This folder looks at making and importing modules in python
