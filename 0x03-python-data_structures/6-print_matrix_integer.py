22a0ddd199433baadd6f70d9c7ba7a79222b705a
Moses <mosemuthoni57@gmail.com>
2023-11-05 15:05:33 +0300

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) < 2:
+        print("")
+        exit(0)
     for i in matrix:
         print("{:d} {:d} {:d}".format(i[0], i[1], i[2]))
