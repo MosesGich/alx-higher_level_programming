9c6e90a6f8f130dce868860ee2bd191960f27fe5
Moses <mosemuthoni57@gmail.com>
2023-11-06 22:54:10 +0300

    even or not

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    myl = [3, 5, 6, 7, 10]
     myl2 = []
-    for i in myl:
+    for i in my_list:
         if i % 2 == 0:
             myl2.append(True)
         else:
