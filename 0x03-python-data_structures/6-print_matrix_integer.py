4c9afc265d6228e41839fd73519350d10f12ba28
Moses <mosemuthoni57@gmail.com>
2023-11-06 22:11:15 +0300

    add tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
         exit(0)
     for i in matrix:
         for j in i:
-            print("{:d} ".format(j), end="")
+            print("{:d}".format(j), end="")
         print("\n", end="")
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 1:
+        a = 0
+        b = 0
+    elif len(tuple_a) == 1:
+        a = tuple_a[0]
+        b = 0
+    else:
+        a = tuple_a[0]
+        b = tuple_a[1]
+    if len(tuple_b) < 1:
+        c = 0
+        d = 0
+    elif len(tuple_b) == 1:
+        c = tuple_b[0]
+        d = 0
+    else:
+        c = tuple_b[0]
+        d = tuple_b[1]
+    z = a + c
+    x = b + d
+    tp3 = (z, x)
+    return tp3
