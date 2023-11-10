98f1ec517474ef029ebca0042ee775f171d31c97
Moses <mosemuthoni57@gmail.com>
2023-11-10 12:05:35 +0300

    add list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    newl = []
+    mysum = 0
+    for n in my_list:
+        if n in newl:
+            continue
+        elsei:
+            newl.append(a)
+            mysum += 1
+    return mysum
